d94d29576df2d9900a8f329e2baac4323169f3c6
bluelovers <codelovers@users.sourceforge.net>
2023-01-02 08:01:53 +0800

    feat: import('@storybook/core-common').StorybookConfig

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,18 @@
-module.exports = {
-  stories: [
-    '../stories/**/*.stories.mdx',
-    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
-  ],
+// @ts-check
+
+/**
+ * @type { import('@storybook/core-common').StorybookConfig }
+ */
+const storybookConfig = {
+  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-}
+  framework: {
+    name: '@storybook/nextjs',
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
+};
+
+module.exports = storybookConfig;
