1483e942d213854491cbac75105783380ed771b1
bluelovers <codelovers@users.sourceforge.net>
2023-02-05 04:21:46 +0800

    .

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,15 +4,39 @@
  * @type { import('@storybook/core-common').StorybookConfig }
  */
 const storybookConfig = {
-  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  stories: [
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)'
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-storysource',
+    'storybook-addon-designs',
+    '@storybook/addon-interactions',
+    'storybook-addon-pseudo-states',
+    '@whitespace/storybook-addon-html',
+    '@bbbtech/storybook-formik/register',
+    'storybook-addon-next-router',
+    'storybook-color-picker',
+    '@geometricpanda/storybook-addon-badges',
+  ],
   framework: {
     name: '@storybook/nextjs',
     options: {}
   },
   docs: {
     autodocs: true
-  }
+  },
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      compilerOptions: {},
+    }
+  },
+  features: {
+    interactionsDebugger: true,
+  },
 };
 
 module.exports = storybookConfig;
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,31 @@
+// @ts-check
+
+import { RouterContext } from 'next/dist/shared/lib/router-context';
+import { cookieDecorator } from 'storybook-addon-cookie';
+
 export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
+  actions: {
+    argTypesRegex: '^on[A-Z].*',
+  },
+  controls: {
+    expanded: true,
+    sort: 'requiredFirst',
+  },
+  html: {
+    prettier: {
+      tabWidth: 2,
+      useTabs: true,
+    },
+    root: '#storybook-root',
+  },
+  nextRouter: {
+    Provider: RouterContext.Provider,
+  },
+  backgrounds: {
+    default: 'light',
+  },
 }
+
+export const decorators = [
+  cookieDecorator,
+];
